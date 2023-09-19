3b3701f0a874af392d1d1ac1b55f6df0564ba3aa
Bruno Ferreira <brunomgferreira17@gmail.com>
2023-09-19 14:44:56 +0100

    header design is done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import GlobalStyle from './theme/GlobalStyle'
+import Theme from './theme/Theme'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+        <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
